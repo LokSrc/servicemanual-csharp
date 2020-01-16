13d33e0471ceedf700cf0ff4731ac6fd226a88bf
LokSrc <lokkimestari@gmail.com>
2020-01-16 16:39:31 +0200

    Added parameters to servicetask object

--- EtteplanMORE.ServiceManual.ApplicationCore/Entities/ServiceTask.cs
+++ EtteplanMORE.ServiceManual.ApplicationCore/Entities/ServiceTask.cs
@@ -1,7 +1,20 @@
-﻿namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
+﻿using System;
+
+namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
 {
+    public enum Criticality
+    {
+        Critical,
+        Important,
+        Mild
+    }
+
     public class ServiceTask
     {
-        // TODO: params
+        public FactoryDevice Target { get; set; }
+        public Criticality Critical { get; set; }
+        public DateTime DateIssued { get; set; }
+        public string Description { get; set; }
+        public bool Closed { get; set; }
     }
 }
--- EtteplanMORE.ServiceManual.Web/Dtos/ServiceTaskDto.cs
+++ EtteplanMORE.ServiceManual.Web/Dtos/ServiceTaskDto.cs
@@ -1,7 +1,13 @@
-﻿namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
+﻿using System;
+
+namespace EtteplanMORE.ServiceManual.ApplicationCore.Entities
 {
     public class ServiceTaskDto
     {
-        // TODO: params
+        public FactoryDevice Target { get; set; }
+        public Criticality Critical { get; set; }
+        public DateTime DateIssued { get; set; }
+        public string Description { get; set; }
+        public bool Closed { get; set; }
     }
 }
